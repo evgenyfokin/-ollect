6711ff91b0ca274080fb47914d8e53ca7423ff10
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 00:17:36 +0300

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL
-console.log(API_URL)
+const API_URL = import.meta.env.VITE_APP_API_URL
+console.log(import.meta.env.VITE_APP_API_URL)
 
 export const register = (userData) => axios.post(`${API_URL}/register`, userData)
 export const login = (userData) => axios.post(`${API_URL}/login`, userData)
