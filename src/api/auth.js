4575f7dbda1607be37a61f10582d357bddbff25c
evgenyfokin <fokin_e@inbox.ru>
2023-07-19 00:25:22 +0300

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_APP_API_URL
 console.log(import.meta.env.VITE_APP_API_URL)
 
-export const register = (userData) => axios.post(`${API_URL}/register`, userData)
-export const login = (userData) => axios.post(`${API_URL}/login`, userData)
+export const register = (userData) => axios.post(`${API_URL}/auth/register`, userData)
+export const login = (userData) => axios.post(`${API_URL}/auth/login`, userData)
